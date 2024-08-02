74f64a9374dc56cba22c86ac4520896e22f0c0f8
geraldine <geraldine.rouy@le-campus-numerique.fr>
2024-08-02 14:12:56 +0200

    Product Model created, controller and view modified

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,15 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-//    public $productList = [1,2,3,4,5,6,7,8,9,10];
     public function index()
     {
-        $products = DB::select('select * from products');
-        return view("product-list",['catalog'=>$products]);
-//        return view("product-list", ["listTitle" => $this->productList]);
+         $products = Product::all();
+         return view("product-list", ["catalog" => $products]);
     }
 
     public function show(int $id)
--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -1,36 +1,32 @@
 @extends('layout')
 @section('content')
 
-
-
-<div class="container mt-5">
-    <div class="row">
-        <div class="col text-center">
-            <h1 class="display-4 font-weight-bold">Product List</h1>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col text-center">
+                <h1 class="display-4 font-weight-bold">Product List</h1>
+            </div>
         </div>
-    </div>
-    <div class="row mt-4">
-        @foreach ($catalog as $products)
-            <div class="col-md-4 mb-4">
-                <div class="card h-100">
-                    <h1 class="text-center">{{$products->name}}</h1>
-                    <p class="text-center text-muted">Browse our exclusive collection of products</p>
-                    <img src="{{$products->productImage}}" class="card-img-top" alt="Product Image">
-                    <div class="card-body">
-                        <h5 class="card-title">Price : {{$products->price}}€</h5>
-                        <p class="card-text">{{$products->productDescription}}</p>
-                        <a href="product/" class="btn btn-primary">View Details</a>
+        <div class="row mt-4">
+            @foreach ($catalog as $product)
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <h1 class="text-center">{{$product->name}}</h1>
+                        <p class="text-center text-muted">Browse our exclusive collection of products</p>
+                        <img src="{{$product->productImage}}" class="card-img-top" alt="Product Image">
+                        <div class="card-body">
+                            <h5 class="card-title">Price : {{$product->price}}€</h5>
+                            <p class="card-text">{{$product->productDescription}}</p>
+                            <a href="product/" class="btn btn-primary">View Details</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
-</div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
 
 @endsection
